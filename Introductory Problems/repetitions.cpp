521212c11f69baefc5b17eba8f1f7f9412a95b24
mdsunbeam <mdsunbeam3.14@gmail.com>
2021-10-17 23:22:52 -0500

    repetitions commit

---
+++ Introductory Problems/repetitions.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    uint64_t rep, cur_rep, max_rep;
+    cin >> str;
+    cur_rep = max_rep = 1;
+    for (uint64_t i = 0; i < str.size(); i++)
+    {
+        if (str[i] == str[i + 1])
+        {
+            cur_rep++;
+        }
+        else
+        {
+            cur_rep = 1;
+        }
+        max_rep = max(cur_rep, max_rep);
+    }
+
+    cout << max_rep << endl;
+}
