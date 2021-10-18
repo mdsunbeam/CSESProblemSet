414ad48a39c8b13fbcd1dbd5f3020e31b1d2e41a
mdsunbeam <mdsunbeam3.14@gmail.com>
2021-10-17 22:05:19 -0500

    format fixed

--- Introductory Problems/missing_number.cpp
+++ Introductory Problems/missing_number.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     uint64_t n, nums, total;
     vector<uint64_t> vec;
     cin >> n;
-    
-    while(cin >> nums){
+
+    while (cin >> nums)
+    {
         vec.push_back(nums);
     }
 
     sort(vec.begin(), vec.end());
-    total = (n)*(n+1)/2;
+    total = (n) * (n + 1) / 2;
 
-    for(uint64_t i = 0; i < vec.size(); i++){
+    for (uint64_t i = 0; i < vec.size(); i++)
+    {
         total -= vec[i];
     }
-    
+
     cout << total << endl;
 }
